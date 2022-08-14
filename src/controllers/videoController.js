6851bfc467ca8d7f8dc959367b4db4ddca03552c
bnetcoder <silr01@naver.com>
2022-08-14 17:22:36 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  return res.send(`see#${req.params.id}`);
-};
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => res.send("delete video");
