c810cd4eb7cbad4dd2e47f8343f80b367157abe3
bnetcoder <silr01@naver.com>
2022-08-19 14:52:15 +0900

    Revert "6.19"
    This reverts commit eb8b3be049158a96f4b6baf1e429c7a2d3d33362.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,10 +4,9 @@ export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = async (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id);
-  res.render("watch", { pageTitle: video.title, video });
+  res.render("watch", { pageTitle: "Watching" });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
@@ -23,20 +22,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = async (req, res) => {
+export const postUpload = (req, res) => {
   const { title, description, hashtags } = req.body;
-  try {
-    await Video.create({
-      title,
-      description,
-      hashtags: hashtags.split(",").map((word) => `${word}`),
-    });
-    return res.redirect("/");
-  } catch (error) {
-    console.log(error);
-    return res.render("upload", {
-      pageTitle: "Upload Video",
-      errorMessage: error._message,
-    });
-  }
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  return res.redirect("/");
 };
