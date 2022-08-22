c3c2db18ab4118fa8ddbe3b55cc16c2a5ba1900d
bnetcoder <silr01@naver.com>
2022-08-22 22:00:27 +0900

    7.1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,17 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {};
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("remove user");
 export const login = (req, res) => res.send("Login");
