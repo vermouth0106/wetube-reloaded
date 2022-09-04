d7868b680337bf7b05eccc1730994a16c85004f4
bnetcoder <silr01@naver.com>
2022-09-04 21:51:12 +0900

    14.5

--- src/server.js
+++ src/server.js
@@ -29,6 +29,11 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
