2a36d791a54dd43916be4c0a2cbdc5b97fbf0c1d
bnetcoder <silr01@naver.com>
2022-09-11 23:42:53 +0900

    github login

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -9,9 +9,6 @@ const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
-
-// 코드 챌린지 요기임니다!!! ♥️💙♥️💙♥️💙♥️💙♥️💙♥️💙♥️💙♥️💙♥️💙
 apiRouter.delete("/comments/:id([0-9a-f]{24})", removeComment);
-// 요기까지 !!!! 😘
 
 export default apiRouter;
